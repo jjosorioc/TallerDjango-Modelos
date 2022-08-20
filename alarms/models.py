601e1a4c63ff7b58be1fe57c7091820820241479
Juan José Osorio <j.osorioc@uniandes.edu.co>
2022-08-20 12:00:02 -0500

    Se creó el app alarms

---
+++ alarms/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from measurements.models import Measurement
+
+
+class Alarm(models.Model):
+    name = models.CharField(max_length=20)
+    measurements = models.ManyToManyField(Measurement,verbose_name="measurements")
+    
+    def __str__(self) -> str:
+        return "{0}: {1}".format(self.name, self.measurements)
