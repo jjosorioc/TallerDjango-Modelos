e342ea96d1ef0bc32184953b5f7a6cc458ba1faf
Juan José Osorio <j.osorioc@uniandes.edu.co>
2022-08-20 11:24:05 -0500

    Variables model

--- variables/models.py
+++ variables/models.py
@@ -1,3 +1,10 @@
+from os import name
 from django.db import models
 
-# Create your models here.
+
+
+class Variable(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return '{}'.format(self.name)
